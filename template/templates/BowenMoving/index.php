04b442a37486c659a8b8d805c61bba83b76954d1
Darren Murray <dmurray@ccistudios.com>
2011-03-15 16:22:09 -0400

    various things thrown together for now

--- template/templates/BowenMoving/index.php
+++ template/templates/BowenMoving/index.php
@@ -1,1 +1,42 @@
-index
+<!DOCTYPE html>
+
+<html>
+	
+<head>
+	
+</head>
+	
+<body>
+	hello world
+	<div id="wrapper">
+		<div id="top">
+			<jdoc:include type="modules" name="topmenu" style="xhtml"/>
+			what
+			<div id="logo1">
+			the	<jdoc:include type="modules" name="logo1" style="xhtml"/>
+			</div><!-- end logo1 -->
+			^$#
+			<div id="logo2">
+			is	<jdoc:include type="modules" name="logo2" style="xhtml"/>
+			</div><!-- end logo2 -->
+			going
+			<div class="clear"></div>	
+		</div>
+			on
+		<div id="content">
+			<jdoc:include type="modules" name="content" style="xhtml"/>
+		</div>
+		figure
+		<div id="bottom">
+			<jdoc:include type="modules" name="bottom" style="xhtml"/>
+		</div>
+		it
+		<div id="footer">
+			<jdoc:include type="modules" name="footer" style="xhtml"/>
+		</div>
+	</div>
+	out		
+		
+</body>
+
+</html>
