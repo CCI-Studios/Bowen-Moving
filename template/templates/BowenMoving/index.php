75d52d470c48fd7f7538fb223ba1f3f60854acae
Darren Murray <dmurray@ccistudios.com>
2011-03-16 16:53:27 -0400

    additional structure/coding

--- template/templates/BowenMoving/index.php
+++ template/templates/BowenMoving/index.php
@@ -3,39 +3,47 @@
 <html>
 	
 <head>
-	
+	<jdoc:include type="head" />
+	<link rel="stylesheet" href="/templates/BowenMoving/css/template.css" />
 </head>
 	
 <body>
-	hello world
-	<div id="wrapper">
-		<div id="top">
-			<jdoc:include type="modules" name="topmenu" style="xhtml"/>
-			what
-			<div id="logo1">
-			the	<jdoc:include type="modules" name="logo1" style="xhtml"/>
-			</div><!-- end logo1 -->
-			^$#
-			<div id="logo2">
-			is	<jdoc:include type="modules" name="logo2" style="xhtml"/>
-			</div><!-- end logo2 -->
-			going
-			<div class="clear"></div>	
-		</div>
-			on
-		<div id="content">
-			<jdoc:include type="modules" name="content" style="xhtml"/>
-		</div>
-		figure
-		<div id="bottom">
-			<jdoc:include type="modules" name="bottom" style="xhtml"/>
-		</div>
-		it
-		<div id="footer">
-			<jdoc:include type="modules" name="footer" style="xhtml"/>
-		</div>
-	</div>
-	out		
+			<div id="top">
+				<div>
+					<jdoc:include type="modules" name="top" style="xhtml"/>
+	
+					<div id="logo1">
+						<jdoc:include type="modules" name="logo1" style="xhtml"/>
+					</div><!-- end logo1 -->
+
+					<div id="logo2">
+						<jdoc:include type="modules" name="logo2" style="xhtml"/>
+					</div><!-- end logo2 -->
+				</div>
+			</div>
+				<div class="clear"></div>	
+						
+			<div id="mainmenu">
+				<jdoc:include type="menu" name="mainmenu" style="xhtml">
+			</div>
+			
+			<div id="content">
+
+			</div>
+
+			<div id="bottom">
+				<jdoc:include type="modules" name="bottom" style="xhtml"/>
+			</div>
+
+			<div id="footer">
+				<div>
+					Copyright &copy; Bowen Moving 2011
+					<div>
+						<jdoc:include type="modules" name="footer" style="xhtml"/>
+					</div>
+				</div>
+			</div>
+	
 		
 </body>
 
