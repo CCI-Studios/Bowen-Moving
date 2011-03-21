da02419480ec38d4463bc3a050bdc1d47e325f0d
Darren Murray <dmurray@ccistudios.com>
2011-03-21 16:59:00 -0400

    more building of the Bowen's Moving site.

--- template/templates/BowenMoving/index.php
+++ template/templates/BowenMoving/index.php
@@ -8,45 +8,43 @@
 </head>
 	
 <body>
-	<div id="top">
-		<div>
-			<jdoc:include type="modules" name="top" style="xhtml"/>
-			<div class="clear"></div>	
+	<div id="top"><div>
+		<jdoc:include type="modules" name="top" style="xhtml"/>
+		<div class="clear"></div>	
+		
+		<div id="logo1">
+			<jdoc:include type="modules" name="logo1" style="xhtml"/>
+		</div><!-- end logo1 -->
 			
-			<div id="logo1">
-				<jdoc:include type="modules" name="logo1" style="xhtml"/>
-			</div><!-- end logo1 -->
-				
-			<div id="logo2">
-				<jdoc:include type="modules" name="logo2" style="xhtml"/>
-			</div><!-- end logo2 -->
-		</div>
-	</div>	
+		<div id="logo2">
+			<jdoc:include type="modules" name="logo2" style="xhtml"/>
+		</div><!-- end logo2 -->
+		<div class="clear"></div>
+	</div></div>	
 						
-	<div id="mainmenu">
-		<div id="main">
-			<jdoc:include type="modules" name="mainmenu" style="xhtml" />
-		</div>
-	</div>
+	<div id="mainmenu"><div>
+		<jdoc:include type="modules" name="mainmenu" style="xhtml" />
+		<div class="clear"></div>
+	</div></div>
 	
-	<div id="content">	
+	<div id="content"><div>
 		<jdoc:include type="component" />
-	</div>
+		<div class="clear"></div>
+	</div></div>
 
-	<div id="bottom">
-		<div>
-			<jdoc:include type="modules" name="bottom" style="xhtml" />
-		</div>
-	</div>
+	<div id="bottom"><div>
+		<jdoc:include type="modules" name="bottom" style="xhtml" />	
+		<div class="clear"></div>
+	</div></div>
 
-	<div id="footer">
+	<div id="footer"><div>
 		<div>
 			<jdoc:include type="modules" name="footer" style="xhtml"/>
 		</div>
-		<div>Copyright &copy; Bowen Moving 2011 
+		<div>Copyright &copy; Bowen Moving 2011<br /> 
 			 <span>Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a></span>
 		</div>
-	</div>
+	</div></div>
 </body>
 
 </html>
