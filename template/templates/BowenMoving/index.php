c639949c793ed0180ea433248e08d77ef01ca9b0
Darren Murray <dmurray@ccistudios.com>
2011-03-30 16:45:54 -0400

    Added Google Analytics

--- template/templates/BowenMoving/index.php
+++ template/templates/BowenMoving/index.php
@@ -16,6 +16,19 @@ $testing = "true";
 </head>
 	
 <body class="<?php echo $menu ?>">
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-22408768-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 	<div id="top"></div>
 		<jdoc:include type="modules" name="top" style="xhtml"/>
 		<div class="clear"></div>	
